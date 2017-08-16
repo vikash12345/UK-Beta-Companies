4830cf4d5cd70a5ebac6f89947fe31daf4864ca1
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-16 11:31:15 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,11 +4,11 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //
 /** looping over list of ids of doctors **/
-for($id = 165314; $id <= 1199999; $id++)
+for($id = 1; $id <= 2; $id++)
 	{
 	 $url = ("https://beta.companieshouse.gov.uk/search?q=a&page=".$id);
 	$link2 = file_get_html($url);
  
- echo $link2;
+ echo $link2;}
  
  ?>
