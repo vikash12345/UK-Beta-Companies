8ed298f0dcdb96e2c9fd7e55c58a854e41535ff4
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-16 11:43:01 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -9,10 +9,12 @@ for($id = 1; $id <= 2; $id++)
 	$link2 = file_get_html($url);
  
 foreach($link2->find("//*[@id='results']")as $element){
-		if(is_object($element))
-		{
-			echo $url 		= $element->find("//*[@id='results']/li[1]/h3/a",0)->href;
-		}
+	
+	echo $element;
+		//if(is_object($element))
+		//{
+		//	echo $url 		= $element->find("//*[@id='results']/li[1]/h3/a",0)->href;
+		//}
 }	
 	 
 
