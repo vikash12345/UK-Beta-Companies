21951f57bab513dc17f877d0adcbf88a1eff8712
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-16 11:39:24 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -8,7 +8,13 @@ for($id = 1; $id <= 2; $id++)
 	 $url = ("https://beta.companieshouse.gov.uk/search?q=a&page=".$id);
 	$link2 = file_get_html($url);
  
- echo $link2;
+foreach($link2->find("//*[@id='results']")as $element){
+		if(is_object($element))
+		{
+			echo $url 		= $element->find("//*[@id="results"]/li[1]/h3/a",0);
+		}
+}	
+	 
 
 }
  
