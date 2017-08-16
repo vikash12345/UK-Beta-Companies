15c869212e1a0d6d29d091bf0fdf0752ee9f7663
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-08-16 16:12:09 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -3,7 +3,7 @@ require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 //
 /** looping over list of ids of doctors **/
-for($id = 1; $id <= 2; $id++)
+for($id = 1; $id <= 1; $id++)
 	{
 	 $url = ("https://beta.companieshouse.gov.uk/search?q=a&page=".$id);
 	$link2 = file_get_html($url);
@@ -13,7 +13,7 @@ foreach($link2->find("//*[@id='results']")as $element){
 	echo $element;
 		if(is_object($element))
 		{
-			echo $url 		= $element->find("//*[@id='results']/li[1]/h3/a")->href;
+			echo $url 		= $element->find("//*[@id='results']/li[1]/h3/a",0)->href;
 		}
 }	
 	 
